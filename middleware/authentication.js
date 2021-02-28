ad2365053c7a01c05bfe7e8cf3114389426f9bfb
Pycrs <petrbohac3@seznam.cz>
2021-02-28 17:16:56 +0100

    Code cleanup, added middleware for admin only endpoints

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 exports.requireLoggedIn = (req, res, next) => {
     const token = req.header('Authorization');
@@ -12,6 +13,16 @@ exports.requireLoggedIn = (req, res, next) => {
         res.status(401).json({success: false, message: 'Invalid or expired token.'})
     }
 }
+
+exports.requireAdmin = async (req, res, next) => {
+    const userId = JSON.parse(Buffer
+        .from(req.header('Authorization').split('.')[1], 'base64')
+        .toString('binary')
+    ).id;
+    const user = await User.findById(userId);
+    if (user.isAdmin) return next();
+    res.status(403).json({success: false, message: 'Not an admin.'})
+};
 
 exports.requireLoggedOut = (req, res, next) => {
     if (req.get('Authorization')) return res.status(401).json({success: false, message: 'You are already logged in.'});
