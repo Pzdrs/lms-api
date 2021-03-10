f82cda6a276a24bf9624adb4defe0d432508c0cc
Pycrs <petrbohac3@seznam.cz>
2021-03-10 19:45:59 +0100

    update

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(express.static('public'));
+app.use(express.static('public/dist'));
 
 // Routes
 app.use('/api', apiRouter);
